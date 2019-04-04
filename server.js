7c0ad904a3028f9cecf71f4cc5d7b248992a3672
lucaszw <lucas.zeer@gmail.com>
2019-04-04 12:11:08 -0400

    listen to process ports

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 app.use(express.static('dist'));
 app.use(express.static('public'));
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(port = process.env.PORT || 3000, () => console.log('Listening on port 3000!'));
